0e5cea16550cceb72665250d8e013effc81546f4
ForsakenKappa <Jafsadcookie@yandex.ru>
2022-05-15 00:51:00 +1100

    Started wokring out the calculation

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const resultBox = document.querySelector('#result');
 const numButtns = document.querySelectorAll('.num-butt');
 const opButtns  = document.querySelectorAll('.op-butt');
 
-let memory = [];
+let memory = '0';
 
 opButtns.forEach(button => button.addEventListener('click', handleOpButtns))
 numButtns.forEach(button => button.addEventListener('click', handleNumButtns));
@@ -15,9 +15,64 @@ function handleNumButtns(e){
 
     resultBox.textContent == '0'? resultBox.textContent = e.target.value : resultBox.textContent += e.target.value;
 }
+
+
+// I don't know what I'm doing
+
 function handleOpButtns(e){
     switch(e.target.value){
         case 'c':
-            resultBox.textContent = '0';
+            clear();
+            memory = '0'
+            break
+        case '+':
+            memory +=  '+';
+            memory += resultBox.textContent;
+            memory = calculate(memory);
+            clear()
+            break
+        case '=':
+            memory += resultBox.textContent;
+            memory = calculate(memory);
+            clear()
+            break;
+            
     }
+
+    resultBox.textContent = memory
+}
+
+function clear(){
+    resultBox.textContent = '0';
+    //memory = '';
+
+}
+
+function calculate(input = ''){
+
+    console.log(input);
+
+    let regExpOp = /[+\-*/]/g
+
+    let numbers = input.split(regExpOp);
+    let operator = input.match(regExpOp);
+
+    let a = parseInt(numbers[0]);
+    let b = parseInt(numbers[1]);
+    let op = operator[0];
+
+    console.log(a)
+    console.log(b)
+    console.log(op)
+
+    if(isNaN(b)) b = 0
+    if(isNaN(a)) a = 0
+
+    switch(op){
+        case '+':
+            return a + b
+    }
+
+
+
 }
