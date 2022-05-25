518dd476f7d9e744e54454030d2447277e615e30
ForsakenKappa <Jafsadcookie@yandex.ru>
2022-05-25 12:45:28 +1100

    Flexible display, witty comments when divide by 0

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ const numButtns = document.querySelectorAll('.num-butt');
 const opButtns  = document.querySelectorAll('.op-butt');
 
 let memory = '';
-let numOfDigitAfterDecimal = 4;
+let clearFlag = false;
+let numOfDigitAfterDecimal = 8;
 
 let decimalPrecision = Math.pow(10, numOfDigitAfterDecimal);
 
@@ -12,18 +13,32 @@ numButtns.forEach(button => button.addEventListener('click', handleNumButtns));
 
 function handleNumButtns(e){
 
-    if(memory.length > 12) return
+    // 60px - default
 
-    if(parseInt(e.target.value) === 0 && memory.length < 2){ // parseInt for e.target.value since there is a '00' button
-        memory = '0';
-    } else if(e.target.value === '.' && (memory.match(/\./g) || !memory)){
-        return
-    } else {
-        memory += e.target.value
-    }
+    if(adjustResultBoxSize() === 1) return
+
+    if(clearFlag) clear()
+
+    if(parseInt(e.target.value) === 0 && memory.length < 2) memory = '0'; // parseInt for e.target.value since there is a '00' buttonm
+    else if(e.target.value === '.' && (memory.match(/\./g) || !memory)) return
+    else memory += e.target.value;
 
     resultBox.textContent = memory;
 
+}
+
+function adjustResultBoxSize(){
+
+    if(memory.length > 30 && memory.length < 80) resultBox.style.fontSize = '16px';
+    else if(memory.length > 26) return 1
+    else if (memory.length > 17) resultBox.style.fontSize = '32px';
+    else if(memory.length > 12) resultBox.style.fontSize = '48px';
+}
+
+function clear(){
+    memory = '';
+    resultBox.style.fontSize = '60px';
+    clearFlag = false;
 }
 
 function handleOpButtns(e){
@@ -31,8 +46,8 @@ function handleOpButtns(e){
     let expression = sliceExpressionString(memory);
     console.log(String(expression) + !!expression)
     
-    if(e.target.value === 'c'){
-        memory = '';
+    if(e.target.value === 'c' || clearFlag){
+        clear()
     }
     else if(e.target.value === '<'){
         memory = memory.slice(0, -1)
@@ -42,6 +57,7 @@ function handleOpButtns(e){
     }
     else if(expression){
         memory = calculate(expression);
+        adjustResultBoxSize()
     }
     else{
 
@@ -59,8 +75,27 @@ function sliceExpressionString(input = ''){
     return expression? expression[0] : null
 }
 
+function makeWittyComment(){
+
+    clearFlag = true;
+    let choice = Math.floor(Math.random() * 5)
+
+    switch(choice){
+        case 0: return 'Division by zero detected'
+        case 1: return 'You did... what?'
+        case 2: return 'Oh no... Division by zero'
+        case 3: return 'Why would you do that?'
+        case 4: return 'I was a young mathematician like you. Then I divided by 0'
+    }
+}
+
 function calculate(expression){
 
-    return String(Math.round(eval(expression) * decimalPrecision) / decimalPrecision)
+    let result = eval(expression)
 
+    if(result === Infinity || isNaN(result)) result = makeWittyComment();
+    else result = String(Math.round( result * decimalPrecision) / decimalPrecision);
+
+    return result
+    
 }
